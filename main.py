45715debcf82bd5d0c04900e79f9b7e65adcdacd
quyencao <boy20195@gmail.com>
2018-04-14 10:41:41 +0700

    sua pso flnn

--- main.py
+++ main.py
@@ -4,49 +4,30 @@ import pso_flnn
 import ga_flnn
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_absolute_error
-import matplotlib.pyplot as plt
+
 
 filename1 = 'data_resource_usage_fiveMinutes_6176858948.csv'
 filename2 = 'data_resource_usage_twoMinutes_6176858948.csv'
 filename3 = 'data_resource_usage_tenMinutes_6176858948.csv'
 filename4 = 'Fuzzy_data_sampling_617685_metric_10min_datetime_origin.csv'
 
-df = pd.read_csv(filename4, header=None, index_col=False, usecols=[0, 1])
+df = pd.read_csv(filename1, header=None, index_col=False, usecols=[3])
 df.dropna(inplace=True)
 
 dataset_original = df.values
 
-X, y = helper.process_data(dataset_original, helper.chebyshev_data, helper.transform, helper.sliding_data, 3, 4)
+# parameters
+sliding_windows = [2, 3, 5]
+c_couple = [(1,2, 1.2), (2, 2), (0.8, 2.0), (1.6, 0.6)]
+pop_sizes = [100, 200, 300, 400]
 
-train_size = int(X.shape[0] * 0.8)
-X_train, y_train, X_test, y_test = X[:train_size, :], y[:train_size, :], X[train_size:, :], y[train_size:, :]
+for sliding in sliding_windows:
+    for pop_size in pop_sizes:
+        for c in c_couple:
 
-X_train = X_train.T
-X_test = X_test.T
-y_train = y_train.T
-y_test = y_test.T
+            p = pso_flnn.PSO(dataset_original, sliding, pop_size, c[0], c[1])
+            # p = ga_flnn.Population()
+            p.train(epochs = 300)
 
-scaler = MinMaxScaler(feature_range=(0,1))
-scaler.fit(dataset_original[:, 0])
-
-p = pso_flnn.PSO()
-# p = ga_flnn.Population()
-best = p.train(X_train, y_train, epochs = 300)
-
-y_pred = best.predict(X_test)
-
-y_pred_inverted = scaler.inverse_transform(y_pred)
-y_test_inverted = scaler.inverse_transform(y_test)
-
-print("MAE: {0}".format(mean_absolute_error(y_pred_inverted, y_test_inverted)))
-
-print(y_test_inverted)
-print(y_pred_inverted)
-print(dataset_original)
-
-plt.plot(y_test_inverted[0, :], color='blue')
-plt.plot(y_pred_inverted[0, :], color='red')
-plt.show()
+            
 
--- pso_flnn.py
+++ pso_flnn.py
@@ -1,8 +1,10 @@
 import numpy as np
 import random
 import copy
-from sklearn.metrics import mean_absolute_error
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 import matplotlib.pyplot as plt
+import helper
 
 class Particle:
     def __init__(self, n_x, n_y):
@@ -78,18 +80,68 @@ class Particle:
 
 
 class PSO:
-    def __init__(self, n_particles=100):
+    def __init__(self, dataset_original, sliding, n_particles=100, c1 = 2, c2 = 2, v_max = 1, v_min = -1):
+        self.dataset_original = dataset_original
         self.n_particles = n_particles
-        self.c1 = 2
-        self.c2 = 2
-        self.v_max = 1
-        self.v_min = -1
+        self.c1 = c1
+        self.c2 = c2
+        self.v_max = v_max
+        self.v_min = v_min
         self.w_max = 0.9
         self.w_min = 0.4
-        self.c1_max = 2.5
-        self.c1_min = 0.5
-        self.c2_max = 2.5
-        self.c2_min = 0.5
+        self.pathsave = 'results/'
+        self.filenamesave = "sliding-{0}-pop_size_{1}-c1_{2}-c2_{3}".format(sliding, n_particles, c1, c2)
+        self.scaler = MinMaxScaler(feature_range=(0,1)).fit(dataset_original[:, 0])
+        self.sliding = sliding
+        # self.c1_max = 2.5
+        # self.c1_min = 0.5
+        # self.c2_max = 2.5
+        # self.c2_min = 0.5
+    
+    def processing_data(self):
+        X, y = helper.process_data(self.dataset_original, helper.chebyshev_data, helper.transform, helper.sliding_data, self.sliding, 4)
+
+        train_size = int(X.shape[0] * 0.8)
+        X_train, y_train, X_test, y_test = X[:train_size, :], y[:train_size, :], X[train_size:, :], y[train_size:, :]
+
+        X_train = X_train.T
+        X_test = X_test.T
+        y_train = y_train.T
+        y_test = y_test.T
+
+        self.X_train = X_train
+        self.X_test = X_test
+        self.y_train = y_train
+        self.y_test = y_test
+        
+    
+    def save_file_csv(self):
+        t1 = np.concatenate( (self.y_test_inverse, self.y_pred_inverse), axis = 1)
+        np.savetxt(self.pathsave + self.filenamesave + ".csv", t1, delimiter=",")
+
+    def predict(self, best_particle):
+        y_pred = best_particle.predict(self.X_test)
+
+        self.y_pred_inverse = self.scaler.inverse_transform(y_pred).T
+        self.y_test_inverse = self.scaler.inverse_transform(self.y_test).T
+
+        self.score_test_MAE = mean_absolute_error(self.y_pred_inverse, self.y_test_inverse)
+        self.score_test_RMSE = np.sqrt(mean_squared_error(self.y_pred_inverse, self.y_test_inverse))
+
+        self.draw_predict()
+
+        self.save_file_csv()
+
+    def draw_predict(self):
+        plt.figure(2)
+        plt.plot(self.y_test_inverse[:, 0])
+        plt.plot(self.y_pred_inverse[:, 0])
+        plt.title('Model predict')
+        plt.ylabel('Real value')
+        plt.xlabel('Point')
+        plt.legend(['realY... Test Score RMSE= ' + str(self.score_test_RMSE) , 'predictY... Test Score MAE= '+ str(self.score_test_MAE)], loc='upper right')
+        plt.savefig(self.pathsave + self.filenamesave + ".png")
+        plt.close()
 
     def initialize_particles(self, n_x, n_y):
         particles = []
@@ -98,7 +150,10 @@ class PSO:
             particles.append(p)
         return particles
 
-    def train(self, X_train, y_train, epochs=450):
+    def train(self, epochs=450):
+        self.processing_data()
+        X_train, y_train = self.X_train, self.y_train
+
         n_x = X_train.shape[0]
         n_y = y_train.shape[0]
 
@@ -106,17 +161,15 @@ class PSO:
         gbest_fitness = -1
         gbest_particle = None
 
-        train_maes = []
-
         particles = self.initialize_particles(n_x, n_y)
 
         for e in range(epochs):
 
             w = self.w_min + (epochs - e) / epochs * (self.w_max - self.w_min)
 
-            c1 = (self.c1_min - self.c1_max) * e / epochs + self.c1_max
+            # c1 = (self.c1_min - self.c1_max) * e / epochs + self.c1_max
 
-            c2 = (self.c2_max - self.c2_min) * e / epochs + self.c2_min
+            # c2 = (self.c2_max - self.c2_min) * e / epochs + self.c2_min
 
             avg_mae_train = 0
 
@@ -135,14 +188,13 @@ class PSO:
                 avg_mae_train += p.get_mae(X_train, y_train)
 
             print("Epoch %.f: %.5f" % (e + 1, avg_mae_train / len(particles)))
-            train_maes.append(avg_mae_train / len(particles))
 
             for p in particles:
                 x = p.get_x()
                 pbest = p.get_pbest()
                 v_o = p.get_v()
 
-                v_n = w * v_o + c1 * random.uniform(0, 1) * (pbest - x) + c2 * random.uniform(0, 1) * (gbest - x)
+                v_n = w * v_o + self.c1 * random.uniform(0, 1) * (pbest - x) + self.c2 * random.uniform(0, 1) * (gbest - x)
 
                 v_n[v_n > self.v_max] = self.v_max
                 v_n[v_n < self.v_min] = self.v_min
@@ -151,6 +203,5 @@ class PSO:
 
                 p.set_v(v_n)
                 p.set_x(x_n)
-        plt.plot(train_maes, color='blue')
-        plt.show()
-        return gbest_particle
+        self.predict(gbest_particle)
+        
